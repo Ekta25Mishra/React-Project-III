751a4fa21a1c2ef2fabd996bb5ad198b3ef169e5
Ekta25Mishra <mishrae3104@gmail.com>
2025-11-09 17:03:40 +0530

    1st day done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
-
+import { useEffect } from 'react';
+import axios from './api/axiosconfig'
 const App = () => {
+  const getproduct = async()=>{
+    try{
+      const res=await axios.get('/products');
+      console.log(res);
+      
+    }
+    catch(error){
+      console.log(error);;
+    }
+  };
+
+  useEffect(()=>{
+    getproduct();
+  },[]);
   return (
     <div>App</div>
   )
